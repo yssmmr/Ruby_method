5a2ea8d76c021568f2c4eb18ffe7a0dd1915b5b3
yssmmr <125275634+yssmmr@users.noreply.github.com>
2023-03-06 11:40:21 +0000

    [Finish]2-9exercise

--- new.rb
+++ new.rb
@@ -1,5 +1,11 @@
 # インスタンスメソッド
 class Car
+
+  def move(direction,distance)
+    self.turn(direction)
+    self.run(distance)
+  end
+
   def turn(direction)
     puts "#{direction}に曲がります"
   end
@@ -7,24 +13,23 @@ class Car
   def run(distance)
     puts "車で#{distance}キロ走ります。"
   end
-
 end
 
 car = Car.new
-car.turn("右")
-
-car = Car.new
-car.run(5)
+car.move("右",5)
 
 
 # クラスメソッド
 
 class Car
+  def self.turn(direction)
+    puts "#{direction}に曲がります。"
+  end
+
   def self.run(distance)
     puts "車で#{distance}キロ走ります。"
   end
 end
 
-Car.run(10)
-
+Car.turn("右")
 
